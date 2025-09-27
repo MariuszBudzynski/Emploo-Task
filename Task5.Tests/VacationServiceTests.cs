412ecb2c16a183ad7a061a9890eb974475d6b405
Hood <mariusz.budzynski87@gmail.com>
2025-09-27 10:31:52 +0200

    Added task 5

---
+++ Task5.Tests/VacationServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Task2.Models;
+using Task4;
+
+namespace Task5.Tests
+{
+    public class VacationServiceTests
+    {
+        private readonly IVacationRequestService _sut;
+        public VacationServiceTests()
+        {
+            _sut = new VacationRequestService();
+        }
+
+        [Fact]
+        public void employee_can_request_vacation()
+        {
+            //Arrange
+            var employee = new Employee { Id = 1, Name = "Jan" };
+            var package = new VacationPackage { Id = 1, GrantedDays = 10, Year = DateTime.Today.Year };
+
+            var vacations = new List<Vacation>
+            {
+                new Vacation
+                {
+                    Id = 1,
+                    EmployeeId = 1,
+                    DateSince = DateTime.Today.AddDays(-5),
+                    DateUntil = DateTime.Today.AddDays(-3),
+                    IsPartialVacation = false
+                }
+            };
+
+            //Act
+            var result = _sut.IfEmployeeCanRequestVacation(employee, vacations, package);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void employee_cant_request_vacation()
+        {
+            //Arrange
+            var employee = new Employee { Id = 1, Name = "Jan" };
+            var package = new VacationPackage { Id = 1, GrantedDays = 2, Year = DateTime.Today.Year };
+
+            var vacations = new List<Vacation>
+            {
+                new Vacation
+                {
+                    Id = 1,
+                    EmployeeId = 1,
+                    DateSince = DateTime.Today.AddDays(-5),
+                    DateUntil = DateTime.Today.AddDays(-4),
+                    IsPartialVacation = false
+                }
+            };
+
+            //Act
+            var result = _sut.IfEmployeeCanRequestVacation(employee, vacations, package);
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
